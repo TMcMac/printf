d864f50ea3a80bf936c3f78ab814a7d7eb6e9730
Jacob Ide <jacobmeide@gmail.com>
2020-03-16 16:42:21 +0000

    Create parsing and if/else functionality

--- _printf.c
+++ _printf.c
@@ -8,9 +8,32 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int i = 0, j = 0;
+	buffer[1024] = {0};
 
-	va_list(args);
+	tp type_dict[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
 
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			/*go get formated args formula*/
+		}
+		else
+		{
+			buffer[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+	write (1, buff, j)
+	va_end(args);
+	return (j);
 }
