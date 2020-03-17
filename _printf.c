c71959042d7ec4ae5ff1d86a1d4f032939be7a5a
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-03-17 06:39:10 +0000

    Honestly three hours of cranking on it...

--- _printf.c
+++ _printf.c
@@ -9,40 +9,42 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int i = 0, j = 0;
-	char buffer[1024] = {0};
+        va_list arg;
+        int inputpos = 0, bufpos = 0, dictinc = 0;
+        char buffer[1024] = {0};
+        int *bufposptr = &bufpos;
 
-	pt type_dict[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{NULL, NULL}
-	};
-
-	va_start(arg, format);
-
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			while (type_dict[j].type != NULL)
-			{
-				if(format[i] == type_dict[j].type[0])
-				{
-					buffer[j] = type_dict[j].f(arg);
-				}
-				j++;
+        pt type_dict[] = {
+                {'c', print_char},
+                {'i', print_int},
+                {'d', print_dec},
+                {'%', print_prct},
+                {NULL, NULL}
+        };
+        if (format == NULL)
+                return (-1);
+        va_start(arg, format);
+        while (format && format[inputpos])
+        {
+                if (format[inputpos] == %)
+                {
+                        inputpos++;
+                        while (type_dict[dictinc].type != NULL)
+                        {
+                                if (format[inputpos] == type_dict[dictinc].type[0])
+                                {
+                                        type_dict[dictinc].f(arg, bufposptr, buffer));
 			}
 		}
-		else
-		{
-			buffer[j] = format[i];
-			j++;
-		}
-		i++;
 	}
-	write (1, buffer, j);
-	va_end(arg);
-	return (j);
+	else
+	{
+		buffer[bufpos] = format[inputpos];
+		inputpos++;
+		bufpos++;
+	}
+}
+write(1, buffer, bufpos);
+va_end(arg);
+return (0);
 }
--- holberton.h
+++ holberton.h
@@ -4,20 +4,29 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
+/**
+ * struct pointer - will be a struct of chars to match %' ' for
+ * print f and function pointers
+ * @type: the chars for printf formats is c,s,i,d,x,p
+ * @f: the function pointers for proper formatting
+ */
 typedef struct pointer
 {
-	char *type;
-	void (*f)();
+        char type;
+        int (*f)(va_list, int *, char *);
 } pt;
 
-int _putchar(char c);
 int _strlen(char *s);
-void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 
 int _printf(const char *format, ...);
-char print_char(va_list arg);
-char print_string(va_list arg);
+int print_char(va_list arg, int *bufposptr, char *bufptr);
+int print_string(va_list arg, int *bufposptr, char *bufptr);
+int print_int(va_list arg, int *bufposptr, char *bufptr);
+int print_dec(va_list arg, int *bufposptr, char *bufptr);
+int print_prct(va_list arg, int *bufpostptr, char *bufptr);
 #endif
+
